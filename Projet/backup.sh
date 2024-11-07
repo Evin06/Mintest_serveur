2443965c88183e803cc34f5aeab7773bda79248c
EvinPara <96207677+Evin06@users.noreply.github.com>
2024-11-07 11:03:37 +0100

    revenir l'idée de base

--- Projet/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Créer une sauvegarde du monde
-tar -czf /minetest/backups/$(date +%Y%m%d%H%M%S).tar.gz /minetest/worlds
-echo "Sauvegarde réalisée le $(date)" >> /minetest/backups/backup.log
